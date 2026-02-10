4bdc1d7786263c50973da33e6ca9d6b5415bc460
mjmandelah07 <aramidemojisolagrace@gmail.com>
2026-02-10 02:31:50 +0100

    Updated

--- components/ui/public-footer.tsx
+++ components/ui/public-footer.tsx
@@ -77,6 +77,11 @@ export function PublicFooter() {
               </a>
             </Button>
 
+            <div className="flex flex-col items-center gap-3">
+              <Image src="/qrcode.png" alt="Scan to open Pools & Pool" width={120} height={120} />
+              <span className="text-sm text-white/80">Scan to open Pools & Pool</span>
+            </div>
+
             <div className="flex items-center gap-4 text-white">
               <Link href="https://www.tiktok.com/@poolsandpool" target="_blank">
                 <button
